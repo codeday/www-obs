41cffb289b5da5f9e064c511617d8f6f93876146
Tyler Menezes <tylermenezes@codeday.org>
2021-02-19 16:08:14 -0500

    add clap and carp

--- src/components/TwitchEmojiOverlay.js
+++ src/components/TwitchEmojiOverlay.js
@@ -6,6 +6,10 @@ import tmi from 'tmi.js';
 const HIDE_TIMEOUT = 2;
 const MAX_OPACITY = 0.8;
 const MAX_HEIGHT = '30%';
+const SPECIAL_EMOJI = {
+  'clap': '/clap.png',
+  'carp': '/carp.png',
+};
 
 export default function TwitchEmojiOverlay({ channel }) {
   const [displayedEmotes, modifyDisplayedEmotes] = useReducer((prev, { action, emotes }) => {
@@ -26,10 +30,17 @@ export default function TwitchEmojiOverlay({ channel }) {
     if (!client || typeof window === 'undefined') return;
     client.connect();
 
-    client.on('message', (_, tags) => {
-      const emotes = Object.keys(tags.emotes || {})
+    client.on('message', (_, tags, message) => {
+      const emoteImages = Object.keys(tags.emotes || {})
         .map((id) => `https://static-cdn.jtvnw.net/emoticons/v1/${id}/2.0`)
+
+      Object.keys(SPECIAL_EMOJI)
+        .filter((k) => message.includes(k))
+        .forEach((k) => emoteImages.push(SPECIAL_EMOJI[k]));
+
+      const emotes = emoteImages
         .map((image, i) => ({ id: `${tags.id}-${i}`, image, left: Math.random(), bottom: Math.random() }));
+
       modifyDisplayedEmotes({ action: 'add', emotes });
       setTimeout(() => modifyDisplayedEmotes({ action: 'remove', emotes }), HIDE_TIMEOUT * 1000);
     });
@@ -75,9 +86,9 @@ export default function TwitchEmojiOverlay({ channel }) {
                 position="absolute"
                 left={`${Math.round(left * 100)}%`}
                 transition={`opacity 1s ease-in-out, bottom ${HIDE_TIMEOUT}s ease-in-out`}
-                bottom={`-${Math.round((bottom + 1) * 10)}px`}
+                bottom={`-${Math.round((bottom + 1) * 2)}rem`}
+                height="1.5rem"
                 {...stateProps[state]}
-                foo={console.log(state)}
               ></Box>
             )}
           </Transition>
